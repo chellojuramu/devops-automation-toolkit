ff4503eeb25f6fc7d362d2ffc8057b16d7a9a6d7
Ramu Chelloju <ramu.chelloju@ardrainfotech.com>
2026-01-28 13:36:09 +0530

    feat: initialize user creation script with input capture

--- bash_scripts/user_creator.sh
+++ bash_scripts/user_creator.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 USERNAME=$1
 echo "the username requested is: $USERNAME"
+if [ -z "$USERNAME" ]; then
+    echo "Error: You forgot to provide a username!"
+    echo "Usage: ./user_creator.sh <username>"
+    exit 1
+fi
